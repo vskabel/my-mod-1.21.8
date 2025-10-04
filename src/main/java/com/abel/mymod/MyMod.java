25df1829601f23dc0cdcd8a9c3d427781bc7669e
vskabel <abel.varga.sterner@gmail.com>
2025-10-04 15:24:13 +0200

    Init

---
+++ src/main/java/com/abel/mymod/MyMod.java
@@ -0,0 +1,15 @@
+package com.abel.mymod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyMod implements ModInitializer {
+	public static final String MOD_ID = "my-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/abel/mymod/mymodClient.java
@@ -0,0 +1,10 @@
+package com.abel.mymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class mymodClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+        
+    }
+}
