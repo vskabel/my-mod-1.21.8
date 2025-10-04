622a55b923787c1b80fff84032f5f25934492f15
vskabel <abel.varga.sterner@gmail.com>
2025-10-04 15:57:26 +0200

    mixin setup +sources

--- src/main/java/com/abel/mymod/mixin/ExampleMixin.java
+++ src/main/java/com/abel/mymod/mixin/ExampleMixin.java
@@ -1,10 +1,11 @@
 package com.abel.mymod.mixin;
 
-import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.server.MinecraftServer;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
