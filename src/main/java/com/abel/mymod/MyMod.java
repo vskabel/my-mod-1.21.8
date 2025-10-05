962989b11ae8ce9589353690fbdf9bc305036520
vskabel <abel.varga.sterner@gmail.com>
2025-10-05 12:22:11 +0200

    Blockok hozaadva

--- src/main/java/com/abel/mymod/MyMod.java
+++ src/main/java/com/abel/mymod/MyMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import com.abel.mymod.block.ModBlocks;
 import com.abel.mymod.item.ModItems;
 
 public class MyMod implements ModInitializer {
@@ -14,5 +14,6 @@ public class MyMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/com/abel/mymod/item/ModItems.java
+++ src/main/java/com/abel/mymod/item/ModItems.java
@@ -12,29 +12,21 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    // Here we will add all our items
-    public static final Item PINK_GARNET = registerItem("pink_garnet",
-        new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MyMod.MOD_ID, "pink_garnet")))));
-    
-    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()
-            .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MyMod.MOD_ID, "raw_pink_garnet")))));
-
-
+    // Items
+    public static final Item PINK_GARNET = registerItem("pink_garnet");
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet");
 
     // Helper method to register items
-    public static Item registerItem(String name, Item item) {
-        Identifier id = Identifier.of(MyMod.MOD_ID, name);
-        return Registry.register(Registries.ITEM, id, item);
+    private static Item registerItem(String name) {
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MyMod.MOD_ID, name));
+        return Registry.register(Registries.ITEM, key, new Item(new Item.Settings().registryKey(key)));
     }
 
-    // Method to register the items
+    // Register items and add to creative tab
     public static void registerModItems() {
-        // MyMod.LOGGER.info("Registering mod items" + MyMod.MOD_ID);
-        // Adding the item to a creative tab
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
         });
     }
-
 }
