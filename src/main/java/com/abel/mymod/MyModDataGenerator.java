2a7bd9fb21a1e0c2118211ac3162b524ad197b39
vskabel <abel.varga.sterner@gmail.com>
2025-10-17 16:47:00 +0200

    datagen hozzaadva

--- src/main/java/com/abel/mymod/MyModDataGenerator.java
+++ src/main/java/com/abel/mymod/MyModDataGenerator.java
@@ -3,6 +3,7 @@ import com.abel.mymod.datagen.ModBlockTagProvider;
 import com.abel.mymod.datagen.ModItemTagProvider;
 import com.abel.mymod.datagen.ModLootTableProvider;
 import com.abel.mymod.datagen.ModModelProvider;
+import com.abel.mymod.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -13,10 +14,11 @@ public class MyModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator generator) {
 		FabricDataGenerator.Pack pack = generator.createPack();
-    pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
 		pack.addProvider(ModBlockTagProvider::new);
-        pack.addProvider(ModLootTableProvider::new);
-        pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 
 }
--- src/main/java/com/abel/mymod/datagen/ModRecipeProvider.java
+++ src/main/java/com/abel/mymod/datagen/ModRecipeProvider.java
@@ -4,12 +4,8 @@ import com.abel.mymod.block.ModBlocks;
 import com.abel.mymod.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.recipe.CookingRecipeJsonBuilder;
-import net.minecraft.data.recipe.RecipeExporter;
-import net.minecraft.data.recipe.RecipeGenerator;
-import net.minecraft.item.Item;
+import net.minecraft.data.recipe.*;
 import net.minecraft.item.ItemConvertible;
-import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 
@@ -40,6 +36,23 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 // Blasting recipe (faster)
                 offerBlasting(PINK_GARNET_SMELTABLES, RecipeCategory.MISC,
                     ModItems.PINK_GARNET, 0.25f, 100, "pink_garnet");
+                offerReversibleCompactingRecipes(RecipeCategory.BUILDING_BLOCKS, ModItems.PINK_GARNET, RecipeCategory.DECORATIONS, ModBlocks.PINK_GARNET_BLOCK);
+
+                // Raw Pink Garnet Block crafting (3x3)
+                createShaped(RecipeCategory.BUILDING_BLOCKS, ModBlocks.RAW_PINK_GARNET_BLOCK)
+                        .pattern("RRR")
+                        .pattern("RRR")
+                        .pattern("RRR")
+                        .input('R', ModItems.RAW_PINK_GARNET)
+                        .criterion(hasItem(ModItems.RAW_PINK_GARNET), conditionsFromItem(ModItems.RAW_PINK_GARNET))
+                        .offerTo(exporter);
+
+                // Raw Pink Garnet from block (shapeless)
+                createShapeless(RecipeCategory.MISC, ModItems.RAW_PINK_GARNET, 9)
+                        .input(ModBlocks.RAW_PINK_GARNET_BLOCK)
+                        .criterion(hasItem(ModBlocks.RAW_PINK_GARNET_BLOCK), conditionsFromItem(ModBlocks.RAW_PINK_GARNET_BLOCK))
+                        .offerTo(exporter);
+
             }
         };
     }
